41478480843c6924b2b71e2a62c21ef6c70e8bf0
CFJ <chenfj@knowbox.cn>
2018-04-05 21:43:27 +0800

    add sgf

---
+++ Assets/Snaker/Modules/ExampleA/UIMainPage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SGF.Codec;
+using SGF.Unity.UI;
+
+public class UIMainPage : UIPage {
+    protected override void OnOpen(object arg = null)
+    {
+        base.OnOpen(arg);
+
+         
+    }
+
+	 
+}
